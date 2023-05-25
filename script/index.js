d13c832246b9fefd453579cc257484e57175b6d1
Nivesh Rohankar <71220063+rohankarnivesh@users.noreply.github.com>
2023-05-25 16:47:20 +0530

    Update index.js

--- script/index.js
+++ script/index.js
@@ -20,11 +20,11 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal('.play-text', {delay:200, origin:'bottom'});
+sr.reveal('.play-text', {delay:250, origin:'bottom'});
 sr.reveal('.play-img', {delay:450, origin:'top'});
 sr.reveal('.icons', {delay:500, origin:'left'});
 sr.reveal('.scroll-down', {delay:300, origin:'bottom'});
-sr.reveal('.what-box', {delay:150, origin:'bottom'});
-sr.reveal('.skill-icon', {delay:250, origin:'top'});
-sr.reveal('.project-box', {delay:150, origin:'bottom'});
-sr.reveal('.send', {delay:150, origin:'top'});
+sr.reveal('.what-box', {delay:250, origin:'bottom'});
+sr.reveal('.skill-icon', {delay:300, origin:'top'});
+sr.reveal('.project-box', {delay:250, origin:'bottom'});
+sr.reveal('.send', {delay:350, origin:'top'});
